e739fb37c614044a3f402f7fa06b27607bf57712
Sam <88557446+JaelMaufes@users.noreply.github.com>
2025-03-31 13:47:50 -0300

    Update script.js carousel

--- js/script.js
+++ js/script.js
@@ -12,16 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
     escreverTitulo();
 
     // Carrossel de Imagens
-    let slideIndex = 0;
     function showSlides() {
-        let slides = document.getElementsByClassName("carousel-slide");
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) { slideIndex = 1; }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 3000); // Muda a imagem a cada 3s
+    let slides = document.querySelectorAll(".carousel-container img");
+    slides.forEach(slide => slide.style.display = "none"); 
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
     }
     showSlides();
 
